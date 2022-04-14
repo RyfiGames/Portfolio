338a5d3aed111438ade5b5546764cc594fd2e3cd
RyfiGames <69179298+RyfiGames@users.noreply.github.com>
2022-04-14 17:23:32 -0400

    Designed sorting selectors

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,7 +8,40 @@ class Home extends React.Component {
         return (
             <div className="Home">
                 <BannerHeader />
-                <h1>Technologies I Use</h1>
+                <h1>My Work</h1>
+                <div className='sortHeader'>Sort By:</div>
+                <div className='sortTags'>
+                    <span className='sortButton'>Relevance</span>
+                    <span className='sortButton'>Most Recent</span>
+                    <span className='sortButton'>Least Recent</span>
+                </div>
+                <div className='sortHeader'>By Project Type</div>
+                <div className='sortTags'>
+                    <span className='sortButton'>Application</span>
+                    <span className='sortButton'>Game Development</span>
+                    <span className='sortButton'>Web Development</span>
+                    <span className='sortButton'>API</span>
+                    <span className='sortButton'>School Work</span>
+                </div>
+                <div className='sortHeader'>By Technology:</div>
+                <div className='sortTags'>
+                    <span className='sortButton'>C#</span>
+                    <span className='sortButton'>Java</span>
+                    <span className='sortButton'>JavaScript</span>
+                    <span className='sortButton'>Python</span>
+                    <span className='sortButton'>Unity3D</span>
+                    <span className='sortButton'>ReactJS</span>
+                    <span className='sortButton'>React Native</span>
+                    <span className='sortButton'>ExpressJS</span>
+                    <span className='sortButton'>VSCode</span>
+                    <span className='sortButton'>IntelliJ</span>
+                    <span className='sortButton'>Git</span>
+                </div>
+                <div className='projectContainer'>a</div>
+                <div className='footer'>
+                    <div className='contact'>Get in touch at <a href='mailto:ramagilton18@hotmail.com'>ramagilton18@hotmail.com</a></div>
+                    <div className='copyright'>© Ryan Magilton 2022</div>
+                </div>
             </div>
             
         );
