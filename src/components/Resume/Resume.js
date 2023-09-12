d8c80b70457512217ff097828fa3af55be523743
RyfiGames <69179298+RyfiGames@users.noreply.github.com>
2023-09-12 17:00:47 -0400

    Update resume link

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Resume.css';
-import NavBar from '../NavBar/NavBar';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Resume.css";
+import NavBar from "../NavBar/NavBar";
 
-const resumeLink = 'https://docs.google.com/document/d/1IYhHxmSRvq8XVji92W7FuL213ogIvOt-Y3nV1h8XIaI/preview';
+const resumeLink =
+  "https://docs.google.com/document/d/1fR34mqLsnQym7Akci5_1TrCSuQd_MbHs59kWLuCm_DY/preview";
 
 const Resume = () => (
   <div className="Resume">
     <NavBar />
-    <div className='spacer2'></div>
-    <iframe className='iframe' src={resumeLink}></iframe>
+    <div className="spacer2"></div>
+    <iframe className="iframe" src={resumeLink}></iframe>
   </div>
 );
 
